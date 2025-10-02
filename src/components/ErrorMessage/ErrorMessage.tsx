7ee6f463ba36a4fb05bbb8ecbb9c386d4fe739bc
Vic <waleria.tuszkowa@gmail.com>
2025-10-02 14:23:04 +0200

    errormessange

--- src/components/ErrorMessage/ErrorMessage.tsx
+++ src/components/ErrorMessage/ErrorMessage.tsx
@@ -5,8 +5,13 @@
 // export default function ErrorMessage(_: ErrorMessageProps) {
 //   return <p className={css.text}>There was an error, please try again...</p>;
 // }
-import css from "./ErrorMessage.module.css";
+import type { ReactElement } from "react";
+import styles from "./ErrorMessage.module.css";
 
-export default function ErrorMessage() {
-  return <p className={css.text}>There was an error, please try again...</p>;
+interface ErrorMessageProps {
+  message: string;
+}
+
+export default function ErrorMessage({ message }: ErrorMessageProps): ReactElement {
+  return <p className={styles.error}>{message}</p>;
 }

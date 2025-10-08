b05a396d07feeba18111b245c12536936fe5fcd6
Vic <waleria.tuszkowa@gmail.com>
2025-10-08 18:54:54 +0200

    dist

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,8 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react-swc'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// ⚠️ назва репозиторію має збігатися з тим, що на GitHub
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/04-react-query/', // <-- дуже важливо!
+base: '/04-react-query/',
 })
